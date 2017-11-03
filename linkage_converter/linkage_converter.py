89bb8c874975d41e228a3b31a000186c0d8728cd
Mehmet Tekman <m.tekman@ucl.ac.uk>
2017-11-03 07:39:39 +0100

    updated linkage converter

--- linkage_converter/linkage_converter.py
+++ linkage_converter/linkage_converter.py
@@ -8,7 +8,7 @@ class Converter:
         self.out_haplo   = "linkage.allegro_haplo"
         self.out_descent = "linkage.allegro_descent"
 
-        self.lod_map     = {}
+        self.lod_array     = {}
         self.haplo_map   = {}
         self.descent_map = {}
 
@@ -21,15 +21,15 @@ class Converter:
         return line.splitlines()[0].split()
     
     # Override
-    def extractLOD(self, file):
+    def extractLOD(self, file1):
         return ""
 
     # Override
-    def extractHaplo(self, file):
+    def extractHaplo(self, file1, extrafile = None):
         return ""
 
     # Override
-    def extractDescent(self, file):
+    def extractDescent(self, file1):
         return ""
     
     def writeLOD(self):
@@ -52,7 +52,7 @@ class Converter:
 class Genehunter(Converter):
 
 
-    def __init__(self, lod = None, haplo = None, descent = None):
+    def __init__(self, lod = None, haplo = None, descent = None, mapfile = None):
         super(self)
         
         if lod is not None:
@@ -60,58 +60,26 @@ class Genehunter(Converter):
             self.writeLOD()
 
         if haplo is not None:
-            self.extractHaplo(haplo)
+            if mapfile is None:
+                print >> sys.stderr, "map file required"
+                exit(-1)
+                
+            self.extractHaplo(haplo, mapfile)
             self.writeHaplo()
 
         if descent is not None:
             self.extractDescent(descent)
             self.writeDescent()
-            
-
-    def extractLOD(self, file):
-
-        pos_lod = {}  # pos → lod
-        gt_line = ""
-        
-        with open(file,'r') as fio:
-
-            # Jump to marker positions
-            line = ""
-            while not line.startswith("Current map ("):
-                line = fio.readline()
-                # iterate
-
-            # Extract gts
-            while not line.startswith("npl"):
-                line = fio.readline()
-                gt_line += line.splitlines()[0] + " "
-
-            # Jump to LOD scoring
-            while not line.startswith("position  LOD score    NPL score  p-value"):
-                line = fio.readline()
-
-            # Extract lod
-            while not line.startswith(" "):
-                gpos, lod, npl, pval, info  = map(float,Converter.tokenizer(line))
-
-                if gpos not in pos_lod:
-                    pos_lod[gpos] = lod
-                else:
-                    # update if new lod is larger
-                    old_lod = pos_lod[gpos]
-                    if lod > old_lod:
-                        pos_lod[gpos] = lod
 
 
-            fio.close()
-
+    def __processMarkers(gt_line, pos_lod):
         # Split marker pos pairs into map
         pos_pairs = gt_line.split()
         if len(pos_pairs) % 2 == 1:
             print >> sys.stderr, "even number of elements"
             exit(-1)
         
-        pos_rsid = smap()
+        pos_rsid = {}
         cum_pos = 0
         ein = 0
         while ein < len(pos_pairs) - 1:
@@ -131,32 +99,108 @@ class Genehunter(Converter):
 
         keys_to_annotate = {}
 
+        # bisect and find closest marker
         for marker_pos in pos_rsid_array:
             marker = pos_rsid[marker_pos]
             lm_ind = bisect_left(pos_lod_array, marker_pos)
             lm_pos = pos_lod_array[lm_ind - 1]
 
+            diff1 = marker_pos - lm_pos
+            diff1 = diff1 if diff1 > 0 else -diff1
+
+            if diff1 > 0.1:
+                continue
+
             if lm_pos not in keys_to_annotate:
                 keys_to_annotate[lm_pos] = marker
             else:
-                if marker_pos - 
+                diff2 = marker_pos - keys_to_annotate[lm_pos]             
+                diff2 = diff2 if diff2 > 0 else -diff2
 
+                if diff1 < diff2:
+                    # update with closer marker
+                    keys_to_annotate[lm_pos] = marker
+
+
+        return keys_to_annotate, pos_lod_array
+
+
+    def extractHaplo(self, hapfile, mapfile):
+        with open(haplfile, 'r') as hio:
+            hio.readline()
+
+            # read data in pairs
+            line = "   "
+            while not line.startswith(" "):
                 
             
+            
 
-        # Produce fam, loc, lod, marker
-        ke
+
+    def extractLOD(self, file):
+
+        pos_lod = {}  # pos → lod
+        gt_line = ""
+        num_peds = 0
         
+        with open(file,'r') as fio:
 
-            
-            
-            
-                
-                
-                
-            
-            
-        
+            # Jump to marker positions
+            line = ""
+            while not line.startswith("Current map ("):
+                line = fio.readline()
+                # iterate
+
+            # Extract gts
+            while not line.startswith("npl"):
+                line = fio.readline()
+                gt_line += line.splitlines()[0] + " "
+
+            # Get num pedigrees
+            while not line.startswith("Totalling pedigrees"):
+                line = fio.readline()
+                # iterate
+
+            # Jump to LOD scoring
+            while not line.startswith("position LOD_score  (alpha, HLOD)"):
+                line = fio.readline()
+
+            # Extract lod
+            while not line.startswith(" "):
+                gpos, lod, alpha, hlod, npl, pval, info  = Converter.tokenizer(line))
+                gpos = float(gpos)
+
+                if lod == "-INFINITY":
+                    lod = -10000
+                lod = float(lod)
+
+                alpha = float(alpha.split("(")[-1])
+                hlod  = float(hlod.split(")")[0])
+
+                npl = float(npl)
+                pval = float(pval)
+
+                # insert
+                if gpos not in pos_lod:
+                    pos_lod[gpos] = [lod, alpha, hlod, npl, pval]
+                else:
+                    # update if new lod is larger
+                    old_lod = pos_lod[gpos][0]
+                    if lod > old_lod:
+                        pos_lod[gpos][0] = lod
+
+            fio.close()
 
 
-        
+        postns_with_closest_markers, sorted_poslod = self.__processMarkers(gt_line, pos_lod)
+
+        # update map with marker info
+        for pos in sorted_poslod:
+            marker = ""
+            if pos in postns_with_closest_markers:
+                marker = postns_with_closest_markers[pos]
+
+            lod, alpha, hlod, npl, pval = pos_lod[pos]
+            
+            self.lod_array.append( (pos, lod, alpha, hlod, marker) )
+            
