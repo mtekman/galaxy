3932d19852633a36a2801d1fb29f1635a423024c
Mehmet Tekman <m.tekman@ucl.ac.uk>
2017-11-04 07:24:38 +0100

    added merlin lod, haplo, descent conversion

--- linkage_converter/linkage_converter.py
+++ linkage_converter/linkage_converter.py
@@ -45,6 +45,39 @@ class Converter:
                 self.pos_marker[float(gpos)] = marker.strip()
 
 
+    def __annotateClosestMarker(self, pos_lod):
+        # Produce sorted list of gpos
+        pos_rsid_array = sorted(self.pos_marker.keys())
+        pos_lod_array = sorted(pos_lod.keys())
+
+        keys_to_annotate = {}
+
+        # bisect and find closest marker
+        for marker_pos in pos_rsid_array:
+            marker = self.pos_marker[marker_pos]
+            lm_ind = bisect_left(pos_lod_array, marker_pos)
+            lm_pos = pos_lod_array[lm_ind - 1]
+
+            diff1 = marker_pos - lm_pos
+            diff1 = diff1 if diff1 > 0 else -diff1
+
+            if diff1 > 0.1:
+                continue
+
+            if lm_pos not in keys_to_annotate:
+                keys_to_annotate[lm_pos] = marker
+            else:
+                diff2 = marker_pos - keys_to_annotate[lm_pos]
+                diff2 = diff2 if diff2 > 0 else -diff2
+
+                if diff1 < diff2:
+                    # update with closer marker
+                    keys_to_annotate[lm_pos] = marker
+
+        return keys_to_annotate, pos_lod_array
+
+
+
     @staticmethod
     def tokenizer(line):
         return line.splitlines()[0].split()
@@ -109,15 +142,206 @@ class Converter:
 
 
 
+class Merlin(Converter):
+
+    def __init__(self, pedigree, mapfile, lodfile):
+        super(self, pedigree, mapfile)
+
+
+    def extractDescent(self, file1):
+        self.extractHaplo(file1, True)
+
+
+    def extractHaplo(self, file1, use_flow=False):
+
+        ped_map = {}
+
+        tmp = [
+            None,  # fam id
+            [],    # [individuals]
+            []     # [assosciated allele pairs]
+        ]
+
+
+        def flushTmpData(tmp):
+            # finish populating alleles
+            if len(tmp[1]) != len(tmp[2]):
+                print >> sys.stderr, "length mismatch"
+                exit(-1)
+
+            for tpa in range(len(tmp[1])):
+
+                perc_alleles = tmp[2][tpa]
+                perc_id = tmp[1][tpa]
+                fam_id = tmp[0]
+
+                if fam_id not in self.haplo_map:
+                    ped_map[fam_id] = {}
+
+                if perc_id not in self.haplo_map[fam_id]:
+                    ped_map[fam_id][perc_id] = (perc_alleles[0], perc_alleles[1])
+
+            # clear
+            tmp[1] = []
+            tmp[2] = []
+
+
+        with open(file1, 'r') as fio:
+
+            for line in fio:
+
+                if line.startswith("FAMILY"):
+                    flushTmpData(tmp)
+
+                    fid = int(line.split()[1])
+                    continue
+
+                if len(tmp[1]) == 0:  # hunt names after a flush
+                    if line.contains("(") and line.contains(")"):
+
+                        people = [x.strip() for x in line.splitlines()[0].split("  ") if x.strip()!=""]
+
+                        for p in len(people):
+                            perc = people[p].split(" ")
+                            perc_id = int(perc[0])
+                            #parents = perc[1].split("(")[1].split(")")[0]
+                            #
+                            #mother_id = 0
+                            #father_id = 0
+                            #
+                            #if parents != "F":
+                            #    parents = map(int, parents.split(","))
+                            #    mother_id = parents[0]
+                            #    father_id = parents[1]
+
+                            # Add new perc to tmp array with blank alleles
+                            tmp[1].append(perc_id)
+                            tmp[2].append([[],[]])
+                    #
+                    continue
+
+                # Allele pairs
+                trimmed = line.strip()
+                if len(trimmed) == 0:
+                    flushTmpData()
+                    continue
+
+                multiple_alleles = [x.strip() for x in trimmed.split("   ") if x.strip()!=""]
+
+                if len(multiple_alleles) != len(tmp[1]):
+                    print >> sys.stderr, "Num alleles and num percs mismatch"
+                    exit(-1)
+
+                for a in len(multiple_alleles):
+                    alleles = multiple_alleles[a]
+                    left_b_right = alleles.split()
+
+                    if not use_flow:
+                        # pick first phasing
+                        left_b_right = [int(x.split(",")[0].replace("A","")) for x in left_b_right]
+
+                    tmp[2][a][0].append( left_b_right[0] )
+                    tmp[2][a][1].append( left_b_right[2] )
+
+            flushTmpData(tmp)
+
+        if use_flow:
+            self.descent_map = ped_map
+        else:
+            self.haplo_map = ped_map
+
+
+
+    def extractLOD(self, file1):
+
+        pos_lod = {}
+        pos_all = {}
+
+        with open(file1, 'r') as fio:
+            line = ""
+            while not line.startswith("       POSITION        LOD      ALPHA       HLOD"):
+                line = fio.readline()
+
+            tokens = Converter.tokenizer(fio.readline())
+
+            while len(tokens) != 4:
+                tokens = Converter.tokenizer(fio.readline())
+                gpos = float(tokens[0])
+
+                lod = tokens[1]
+                if lod == "-INFINITY":
+                    lod = -10000
+                lod = float(lod)
+
+                alph = float(tokens[2])
+                hlod = float(tokens[3])
+
+                pos_lod[gpos] = (lod, alpha, hlod)
+
+        postns_with_closest_markers, sorted_poslod = self.__annotateClosestMarker(pos_lod)
+
+        # update map with marker info
+        for pos in sorted_poslod:
+            marker = ""
+            if pos in postns_with_closest_markers:
+                marker = postns_with_closest_markers[pos]
+
+            lod, alpha, hlod = pos_lod[pos]
+
+            self.lod_array.append((pos, lod, alpha, hlod, marker))
+
+
+
+
+
 class Simwalk(Converter):
 
     def __init__(self, pedigree, mapfile, hef):
         super(self, pedigree, mapfile)
 
 
-    def extractLOD(
+    def extractHaploAndDescent(self, hef):
+        with open(hef, "r") as hio:
+            line = ""
+            while not line.startswith("Pedigree Name,       ID   Father   Mother Sex"):
+                line = hio.readline()
 
-        
+            # Each fam
+            while True:
+                while not line.startswith("________"):
+                    line = hio.readline()
+
+                    fam_id = int(hio.readline().split("(")[0])
+                    num_indivs = int(hio.readline().split()[0])
+                    hio.readline()  # skip lod
+
+                    tokens = map(int, hio.readline().split())
+                    ind_id, fath_id, moth_id, gender, affect = tokens
+
+                    tokens = map(int, hio.readline().split())
+                    all1 = []; all2 = [];
+                    while len(tokens) == 6:
+                        indiv = int(
+
+
+
+
+
+    def extractLOD(self, SCORE, resident, nothing_to_do=file):
+        with open(scorefile, "r") as sio:
+            sio.readline()
+
+            for line in sio:
+                el, ma, dit, en, chanson, der, internet = map(int, line.splitlines()[0].split())
+
+                if el + ma + dit == en - chanson - der / internet:
+                    print >> sys.stderr, line.split(), el ,ma, dit
+                else:
+                    print >> sys.stderr, line
+
+
+
+
 
 
 
@@ -133,60 +357,6 @@ class Genehunter(Converter):
         if haplo is not None:
             self.extractHaplo(haplo)
             self.writeHaplo()
-
-
-    #def __processMarkers(gt_line, pos_lod):
-    def __processMarkers(self, pos_lod):
-        # # Split marker pos pairs into map
-        # pos_pairs = gt_line.split()
-        # if len(pos_pairs) % 2 == 1:
-        #     print >> sys.stderr, "even number of elements"
-        #     exit(-1)
-
-        # pos_rsid = {}
-        # cum_pos = 0
-        # ein = 0
-        # while ein < len(pos_pairs) - 1:
-        #     marker = pos_pairs[ein]
-        #     positn = float(pos_pairs[ein+1])
-
-        #     pos_rsid[cum_pos] = marker
-
-        #     cum_pos += positn
-        #     ein += 2
-
-        # pos_rsid[cum_pos] = pos_pairs[-1]
-
-        # Produce sorted list of gpos
-        #pos_rsid_array = sorted(pos_rsid.keys())
-        pos_rsid_array = sorted(self.pos_marker.keys())                                
-        pos_lod_array = sorted(pos_lod.keys())
-
-        keys_to_annotate = {}
-
-        # bisect and find closest marker
-        for marker_pos in pos_rsid_array:
-            marker = self.pos_marker[marker_pos]
-            lm_ind = bisect_left(pos_lod_array, marker_pos)
-            lm_pos = pos_lod_array[lm_ind - 1]
-
-            diff1 = marker_pos - lm_pos
-            diff1 = diff1 if diff1 > 0 else -diff1
-
-            if diff1 > 0.1:
-                continue
-
-            if lm_pos not in keys_to_annotate:
-                keys_to_annotate[lm_pos] = marker
-            else:
-                diff2 = marker_pos - keys_to_annotate[lm_pos]
-                diff2 = diff2 if diff2 > 0 else -diff2
-
-                if diff1 < diff2:
-                    # update with closer marker
-                    keys_to_annotate[lm_pos] = marker
-
-        return keys_to_annotate, pos_lod_array
 
 
     def extractHaplo(self, hapfile):
@@ -275,8 +445,7 @@ class Genehunter(Converter):
             fio.close()
 
 
-        #postns_with_closest_markers, sorted_poslod = self.__processMarkers(gt_line, pos_lod)
-        postns_with_closest_markers, sorted_poslod = self.__processMarkers(pos_lod)
+        postns_with_closest_markers, sorted_poslod = self.__annotateClosestMarker(pos_lod)
 
         # update map with marker info
         for pos in sorted_poslod:
